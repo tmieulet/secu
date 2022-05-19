8508e14f0324398e18e64d3c935b0be9201ecf6e
tmieulet <17229943+tmieulet@users.noreply.github.com>
2022-05-19 08:52:17 +0200

    Update listFiles_lang.php

--- listFiles_lang.php
+++ listFiles_lang.php
@@ -4,5 +4,9 @@ $files = scandir($path);
 $files = array_diff(scandir($path), array('.', '..'));
 foreach($files as $file){
   echo "<a href='$file'>$file</a>";
+  if(is_file($file)){
+  $homepage = file_get_contents($file);
+echo "<p>$homepage</p>";
+  }
 }
 ?>
